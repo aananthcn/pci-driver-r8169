b8085492c112cbf3c190d03255375207ed028619
Aananth C N <c.n.aananth@gmail.com>
2023-11-01 23:06:47 +0530

    Added support to read PCI config registers

--- pci-tools/pci-commands.c
+++ pci-tools/pci-commands.c
@@ -83,12 +83,29 @@ void cmd_print_configs(const char dev_addr[]) {
 	printf("Bytes read = %d\n", bytes_read);
 	dump_buffer_hex(buffer, bytes_read);
 
-	// read PCI configs
-	unsigned short vendor_id = buffer[0] | (buffer[1] << 8);
-	unsigned short device_id = buffer[2] | (buffer[3] << 8);
-	unsigned short cmd_reg   = buffer[4] | (buffer[5] << 8);
-	unsigned short stat_reg  = buffer[6] | (buffer[7] << 8);
-	unsigned char  rev_id    = buffer[8];
+	// read PCI configs - PCIe bytes are always Little Endian
+	unsigned short vendor_id  = buffer[0] | (buffer[1] << 8);
+	unsigned short device_id  = buffer[2] | (buffer[3] << 8);
+	unsigned short cmd_reg    = buffer[4] | (buffer[5] << 8);
+	unsigned short stat_reg   = buffer[6] | (buffer[7] << 8);
+	unsigned char  rev_id     = buffer[8];
+	unsigned int   class_code = buffer[9] | (buffer[0xa] << 8) | (buffer[0xb] << 16);
+	unsigned char  cache_line = buffer[0xc];
+	unsigned char  lat_timer  = buffer[0xd];
+	unsigned char  hdr_type   = buffer[0xe];
+	unsigned char  bist       = buffer[0xf];
+	unsigned int   bar0       = buffer[0x10] | (buffer[0x11] << 8) | (buffer[0x12] << 16) | (buffer[0x13] << 24);
+	unsigned int   bar1       = buffer[0x14] | (buffer[0x15] << 8) | (buffer[0x16] << 16) | (buffer[0x17] << 24);
+	unsigned int   bar2       = buffer[0x18] | (buffer[0x19] << 8) | (buffer[0x1a] << 16) | (buffer[0x1b] << 24);
+	unsigned int   bar3       = buffer[0x1c] | (buffer[0x1d] << 8) | (buffer[0x1e] << 16) | (buffer[0x1f] << 24);
+	unsigned int   bar4       = buffer[0x20] | (buffer[0x21] << 8) | (buffer[0x22] << 16) | (buffer[0x23] << 24);
+	unsigned int   bar5       = buffer[0x24] | (buffer[0x25] << 8) | (buffer[0x26] << 16) | (buffer[0x27] << 24);
+	unsigned int   card_bus_p = buffer[0x28] | (buffer[0x29] << 8) | (buffer[0x2a] << 16) | (buffer[0x2b] << 24);
+	unsigned short subsys_vid = buffer[0x2c] | (buffer[0x2d] << 8);
+	unsigned short subsys_did = buffer[0x2e] | (buffer[0x2f] << 8);
+	unsigned int   exp_rom_ba = buffer[0x30] | (buffer[0x31] << 8) | (buffer[0x32] << 16) | (buffer[0x33] << 24);
+	unsigned char  irq_line   = buffer[0x3c];
+	unsigned char  irq_pin    = buffer[0x3d];
 
 	// free the buffer allocated by read_binary_file function
 	if (buffer)
@@ -96,9 +113,25 @@ void cmd_print_configs(const char dev_addr[]) {
 
 	// print configs
 	printf("\n");
-	printf("Vendor ID   : 0x%X\n", vendor_id);
-	printf("Device ID   : 0x%X\n", device_id);
-	printf("Command Reg : 0x%X\n", cmd_reg);
-	printf("Status Reg  : 0x%X\n", stat_reg);
-	printf("Revision ID : %d\n", rev_id);
+	printf("Vendor ID       : 0x%X\n", vendor_id);
+	printf("Device ID       : 0x%X\n", device_id);
+	printf("Command Reg     : 0x%X\n", cmd_reg);
+	printf("Status Reg      : 0x%X\n", stat_reg);
+	printf("Revision ID     : %d\n", rev_id);
+	printf("Class Code      : 0x%X\n", class_code);
+	printf("Cache Line      : 0x%X\n", cache_line);
+	printf("Latency Timer   : 0x%X\n", lat_timer);
+	printf("Header Type     : 0x%X\n", hdr_type);
+	printf("BIST            : 0x%X\n", bist);
+	printf("BAR0            : 0x%X\n", bar0);
+	printf("BAR1            : 0x%X\n", bar1);
+	printf("BAR2            : 0x%X\n", bar2);
+	printf("BAR3            : 0x%X\n", bar3);
+	printf("BAR4            : 0x%X\n", bar4);
+	printf("BAR5            : 0x%X\n", bar5);
+	printf("Cardbus CIS Ptr : 0x%X\n", card_bus_p);
+	printf("Subs. Vendor ID : 0x%X\n", subsys_vid);
+	printf("Subs. Device ID : 0x%X\n", subsys_did);
+	printf("IRQ Line        : 0x%X\n", irq_line);
+	printf("IRQ Pin         : 0x%X\n", irq_pin);
 }
